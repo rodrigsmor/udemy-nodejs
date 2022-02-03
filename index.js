7b73d5346ab9f38ad23e7eada6efcbfae9c91a0c
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-03 12:03:29 -0300

    Aula 4 - Cria função de detectar diferentes urls

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ let server = http.createServer((req, res) => {
     console.log('URL: ', req.url);
     console.log('METHOD: ', req.method);
 
-    res.end('OK');
+    switch (req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html')
+            res.end('<h1>Olá</h1>');
+        break;
+
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'hcode',
+                    email: 'contatos@hcode.com.br',
+                    id: 1
+                }]
+            }));
+        break;
+    }
 });
 
 server.listen(3000, '127.0.0.1', () => {
