0f9caf371f3f70f6ef26e2a8df979ea3f9779489
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-04 09:08:10 -0300

    Aula 8 - Instala Consign para lidar com as rotas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
-const express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Olá</h1>');
-});
-
-module.exports = routes;
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<h1>Olá</h1>');
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,21 @@
-const express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users: [{
-            name: 'Hcode',
-            email: 'contatos@hcode.com.br',
-            id: 1
-        }]
+module.exports = (app) => {
+    app.get('/users', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.json({
+            users: [{
+                name: 'Hcode',
+                email: 'contatos@hcode.com.br',
+                id: 1
+            }]
+        });
+    })
+    
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: []
+        });
     });
-})
-
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
-    });
-});
-
-module.exports = routes;
+};
