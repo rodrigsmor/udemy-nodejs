72facbba24825724e56090da595d8bfc32d1e2c9
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-04 10:11:47 -0300

    Aula 11 - Consulta banco de dados e Lista usuários

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,21 @@ let db = new NeDB({
 
 module.exports = (app) => {
     app.get('/users', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json')
-        res.json({
-            users: [{
-                name: 'Hcode',
-                email: 'contatos@hcode.com.br',
-                id: 1
-            }]
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json')
+                res.json({
+                    users
+                });
+            }
         });
+
     })
     
     app.post('/users', (req, res) => {  
@@ -27,6 +33,6 @@ module.exports = (app) => {
             } else {
                 res.status(200).json(user);
             }
-        } );
+        });
     });
 };
